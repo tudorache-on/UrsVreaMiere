60533624a008624af739f8a4aa82c92f7f655f8d
tudorache-on <105102365+tudorache-on@users.noreply.github.com>
2023-06-04 00:48:13 +0300

    Am balansat joaca

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ function spawnEnemies() {
     });
 
     if (progress >= 1000) progress-=100;
-    if (count == 15) setInterval(spawnEnemies, progress);
+    if (count == 15) setInterval(spawnEnemies, 3000);
 
     
     target.append(enemy);
